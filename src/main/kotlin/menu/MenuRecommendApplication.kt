537270c5aafe789f12073dcebe6217bf5348e2f1
jaeyoung <kjy0302014@gmail.com>
2023-02-05 14:12:16 +0900

    MenuRecommendService, MenuRecommendApplication 생성

---
+++ src/main/kotlin/menu/MenuRecommendApplication.kt
@@ -0,0 +1,6 @@
+package menu
+
+class MenuRecommendApplication(private val menuRecommendView: MenuRecommendView,
+                               private val menuRecommendService: MenuRecommendService) {
+
+}
